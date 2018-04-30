7854ecbfe2aa87dafdf2fb7978468df7b56a092c
Greg Orvik <gvorvik@gmail.com>
2018-04-30 16:22:47 -0500

    make a swapi request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,8 +2,23 @@ console.log('clientjs sourced');
 
 var app = angular.module('APIApp', []);
 
-app.controller('APIAppController', ['$http', function($http) {
+app.controller('APIAppController', ['$http', function ($http) {
     console.log('API Controller loaded');
-    
+
     var self = this;
+
+    self.message = "What's up Greg?";
+
+    $http({
+        method: 'GET',
+        url: 'https://swapi.co/api/starships/?search=falcon'
+    })
+    .then(function(response) {
+        console.log('SWAPI response', response.data);
+    })
+    .catch(function(error) {
+        console.log('The force was not with us', error);
+    });
+
+
 }]);
